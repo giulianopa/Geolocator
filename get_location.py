c80d84ca0a722a3b72a4fd370dc5b3e5c7b59a72
giulianopa <giuliano.pasqualotto@gmail.com>
2018-08-13 12:39:46 -0700

    Save platform

--- get_location.py
+++ get_location.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2018 Giuliano Pasqualotto (github.com/giulianopa)
 # This code is licensed under MIT license (see LICENSE.txt for details)
 #
+import platform
 import datetime
 import geocoder
 import decimal
@@ -30,12 +31,15 @@ if __name__ == "__main__":
     location = get_location()
     time = get_utc_time()
     hostname = str(socket.gethostname())
+    plat = platform.platform()
 
     # Connect to dynamodb
     client = boto3.client('dynamodb')
 
     # Create JSON
-    data = {'time': {'S': time}, 'hostname': {'S': hostname}, 'location': {'SS': [ str(location[0]), str(location[1]) ]}}
+    data = {'time': {'S': time}, 'hostname': {'S': hostname},\
+        'location': {'SS': [ str(location[0]), str(location[1]) ]},\
+        'platform': {'S': plat}}
 
     # Update table
     response = client.put_item(TableName='DevLocation', Item=data)
